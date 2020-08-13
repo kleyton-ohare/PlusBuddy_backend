99a038c0c96eba5ff3a5f8487e30ca4cc5fa878b
kleyton-ohare <kleyton.ohare@gmail.com>
2020-08-13 19:32:07 +0100

    getting the connection right

--- config/firestore.js
+++ config/firestore.js
@@ -1,20 +1,12 @@
 const admin = require('firebase-admin');
-// const serviceAccount = require('../secure/plusbuddy-key.json');
+const serviceAccount = require('../secure/my_secret.json');
 
 // Connection with the database
 admin.initializeApp({
-    // credential: admin.credential.cert(serviceAccount)
-    credential: admin.credential.applicationDefault()
+    credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = {
     FieldValue: admin.firestore.FieldValue,
     db: admin.firestore()
 }
-
-// Initialize the database by either
-// using the file stored on line 2 and the command on line 6
-// or (which is more secure)
-// by setting an env. variable in PowerShell
-// $env:GOOGLE_APPLICATION_CREDENTIALS="C:\Users\$USERNAME$\Desktop\PlusBuddy\backend\secure\plusbuddy-key.json"
-// and the command on line 7
