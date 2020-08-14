5ca51a83064ae01a965793b27605f7f90257b269
kleyton-ohare <kleyton.ohare@gmail.com>
2020-08-14 12:03:11 +0100

    fixed the env name

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secrets
+mkdir $HOME/secure
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 sudo gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output $HOME/secrets/my_secret.json my_secret.json.gpg
+--output $HOME/secure/my_secret.json my_secret.json.gpg
