8e19fd2c1b3a2b58efcbfe334786aaa945ad304b
kleyton-ohare <kleyton.ohare@gmail.com>
2020-08-14 12:43:24 +0100

    test

--- .github/workflows/decrypt_secret.sh
+++ .github/workflows/decrypt_secret.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/secure
+#mkdir $HOME/secure
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 
-echo "passphras: $SECRET_PASSPHRASE"
+echo "hello from decrypt_secret.sh"
+#echo "passphrase: $SECRET_PASSPHRASE"
 #gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 #--output $HOME/secure/my_secret.json my_secret.json.gpg
