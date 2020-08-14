7a704485ef5bc2c2425c1c1cfa8c16fafe9eb060
kleyton-ohare <kleyton.ohare@gmail.com>
2020-08-14 13:09:22 +0100

    test

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -5,6 +5,5 @@
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 
-echo "passphrase: $SECRET_PASSPHRASE"
-#gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
-#--output $HOME/secure/my_secret.json my_secret.json.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+--output $HOME/secure/my_secret.json my_secret.json.gpg
