b9684776e091caebb1f237edc87eff713b5a11e9
kleyton-ohare <kleyton.ohare@gmail.com>
2020-08-14 11:51:34 +0100

    added sudo

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,5 +4,5 @@
 mkdir $HOME/secure
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+sudo gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output $HOME/secure/my_secret.json my_secret.json.gpg
