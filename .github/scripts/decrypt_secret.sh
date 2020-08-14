e837945018b0237b692b8f5f52bea43166b0a1f8
kleyton-ohare <kleyton.ohare@gmail.com>
2020-08-14 13:55:26 +0100

    test

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,23 +1,9 @@
 #!/bin/sh
 
 # Decrypt the file
-mkdir $HOME/runner/secure
-mkdir testing1234
+mkdir secure
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
---output $HOME/runner/secure/my_secret.json my_secret.json.gpg
-
-echo "pwd"
-pwd
-ls -al
-echo "cd home/runner"
-cd home/runner
-echo "pwd"
-pwd
-ls -al
-
-echo "cd ~/runner"
-cd ~/runner
-ls -al
+--output secure/my_secret.json my_secret.json.gpg
